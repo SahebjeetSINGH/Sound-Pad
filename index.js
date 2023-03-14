4ed40cfcd79866a4c40dcd32a20c7840ea17edd2
Sahebjeet singh <sahebjeet2001@gmail.com>
2023-03-15 03:41:17 +0530

    final

---
+++ index.js
@@ -0,0 +1,36 @@
+const sounds= [
+    'BEAT1','BEAT2',
+    'BRASS','WHISTLE',
+    'PLUCKS','STRINGS',
+    'SYNTH','VOX','RIZZ'
+];
+
+    sounds.forEach((sound)=>{
+    const btn=document.createElement('button');
+    btn.classList.add('btn');
+
+    btn.innerText=sound;
+    let flag=1;
+    btn.addEventListener('click',()=>{
+       
+        const demo=document.getElementById(sound);
+        if ( flag==1)
+        {
+            demo.play();
+            demo.loop=true;
+            flag=2;
+        }
+        else{
+            demo.loop=false;
+            
+            demo.currentTime = 0;
+            demo.pause();
+            
+
+            flag=1;
+        }
+        console.log(demo);
+        console.log(sound);
+    });
+    document.getElementById('buttons').appendChild(btn);
+});
