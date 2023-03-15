f33942e9d9dcf8c5aed45eb0db0abb298ade230b
Sahebjeet singh <sahebjeet2001@gmail.com>
2023-03-15 22:36:38 +0530

    added sound

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const sounds= [
-    'BEAT1','BEAT2',
-    'BRASS','WHISTLE',
-    'PLUCKS','STRINGS',
-    'SYNTH','VOX','RIZZ'
+const sounds = [
+  "BEAT1",
+  "BEAT2",
+  "BRASS",
+  "WHISTLE",
+  "PLUCKS",
+  "STRINGS",
+  "SYNTH",
+  "VOX",
+  "RIZZ",
 ];
+//Changed sound
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
 
-    sounds.forEach((sound)=>{
-    const btn=document.createElement('button');
-    btn.classList.add('btn');
+  btn.innerText = sound;
+  let flag = 1;
+  btn.addEventListener("click", () => {
+    const demo = document.getElementById(sound);
+    if (flag == 1) {
+      demo.play();
+      demo.loop = true;
+      flag = 2;
+    } else {
+      demo.loop = false;
 
-    btn.innerText=sound;
-    let flag=1;
-    btn.addEventListener('click',()=>{
-       
-        const demo=document.getElementById(sound);
-        if ( flag==1)
-        {
-            demo.play();
-            demo.loop=true;
-            flag=2;
-        }
-        else{
-            demo.loop=false;
-            
-            demo.currentTime = 0;
-            demo.pause();
-            
+      demo.currentTime = 0;
+      demo.pause();
 
-            flag=1;
-        }
-        console.log(demo);
-        console.log(sound);
-    });
-    document.getElementById('buttons').appendChild(btn);
+      flag = 1;
+    }
+    console.log(demo);
+    console.log(sound);
+  });
+  document.getElementById("buttons").appendChild(btn);
 });
